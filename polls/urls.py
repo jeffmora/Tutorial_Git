08224c41a05cd5e1b1184366271ed94a9fd64555
jeffmora <67877309+jeffmora@users.noreply.github.com>
2020-07-14 19:20:35 -0500

    Vistas y plantillas en Django

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('<int:pregunta_id>/', views.detalle, name='detalle'),
+    path('<int:pregunta_id>/resultados/', views.resultados, name='resultados'),
+    path('<int:pregunta_id>/voto/', views.voto, name='voto'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 
+from .models import Pregunta
 
 def inicio(request):
-    return HttpResponse("Hola mundo, Está es la aplicación inicial de encuestas.")
-# Create your views here.
+    ultima_lista_preguntas = Pregunta.objects.order_by('-fecha_publicacion')[:5]
+    contexto = {'ultima_lista_preguntas': ultima_lista_preguntas}
+    return render(request, 'polls/indice.html', contexto)
+
+def detalle(request, pregunta_id):
+    pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
+    return render(request, 'polls/detalle.html', {'Pregunta': Pregunta})
+
+def resultados(request, pregunta_id):
+    response = "Estas viendo los resultados de la pregunta %s."
+    return HttpResponse(response % pregunta_id)
+
+def voto(request, pregunta_id):
+    return HttpResponse("Estás votando por la pregunta %s." % pregunta_id)
