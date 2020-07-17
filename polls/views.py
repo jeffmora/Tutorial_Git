1720e4c3166c343c5d72d7c1f98a5b96570d6d95
jeffmora <67877309+jeffmora@users.noreply.github.com>
2020-07-16 19:58:36 -0500

    Contando votos - tutorial Django

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Pregunta
+from .models import Pregunta, Eleccion
 
 def inicio(request):
     ultima_lista_preguntas = Pregunta.objects.order_by('-fecha_publicacion')[:5]
@@ -10,11 +11,26 @@ def inicio(request):
 
 def detalle(request, pregunta_id):
     pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
-    return render(request, 'polls/detalle.html', {'Pregunta': pregunta})
+    return render(request, 'polls/detalle.html', {'pregunta': pregunta})
 
 def resultados(request, pregunta_id):
-    response = "Estas viendo los resultados de la pregunta %s."
-    return HttpResponse(response % pregunta_id)
+    pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
+    return render(request, 'polls/resultados.html', {'pregunta': pregunta})
 
 def voto(request, pregunta_id):
-    return HttpResponse("Estás votando por la pregunta %s." % pregunta_id)
+    pregunta = get_object_or_404(Pregunta, pk=pregunta_id)
+    try:
+        eleccion_elegida = pregunta.eleccion_set.get(pk=request.POST['eleccion'])
+    except (KeyError, Eleccion.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detalle.html', {
+            'pregunta': pregunta,
+            'mensaje_error': "No seleccionaste una opción.",
+        })
+    else:
+        eleccion_elegida.votos += 1
+        eleccion_elegida.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:resultados', args=(pregunta.id,)))
